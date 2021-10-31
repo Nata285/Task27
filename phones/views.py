26967ea85893535803c7863f92c28a599b11a1cb
Наталья <Lisicina_natasha@bk.ru>
2021-10-31 19:09:05 +0400

    Change 31.10

--- phones/views.py
+++ phones/views.py
@@ -1,3 +1,6 @@
+from audioop import reverse
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from phones.models import Phone
@@ -5,12 +8,23 @@ from phones.models import Phone
 
 def show_catalog(request):
     template = 'catalog.html'
-    phone_catalog=Phone.object.all()
-    context = {'phone_catalog': phone_catalog}
+    phone_catalog=Phone.objects.all()
+    if request.GET.get('sort') == 'min_price':
+        phone_catalog = Phone.objects.all().order_by('price')
+    elif request.GET.get('sort') == 'max_price':
+        phone_catalog = Phone.objects.all().order_by('-price')
+    else:
+        # request.GET.get('sort') == 'name':
+        phone_catalog = Phone.objects.all().order_by('name')
+
+    context = {'phone_catalog':phone_catalog}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
+
+
     context = {}
     return render(request, template, context)
+
